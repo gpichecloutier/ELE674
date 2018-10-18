157a57622965f8405b4914f633411c9ab5f4e382
Gabriel Piché Cloutier <gab_pc@live.ca>
2018-10-18 09:28:18 -0400

    First commit

---
+++ Debug/src/Attitude.d
@@ -0,0 +1,7 @@
+src/Attitude.d src/Attitude.o: ../src/Attitude.c \
+ /home/gabriel/Documents/ELE674-01/Lab3/DroneFirmware-Lab/include/Attitude.h \
+ /home/gabriel/Documents/ELE674-01/Lab3/DroneFirmware-Lab/include/Sensor.h
+
+/home/gabriel/Documents/ELE674-01/Lab3/DroneFirmware-Lab/include/Attitude.h:
+
+/home/gabriel/Documents/ELE674-01/Lab3/DroneFirmware-Lab/include/Sensor.h:
